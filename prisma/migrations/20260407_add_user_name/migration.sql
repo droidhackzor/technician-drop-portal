a094ad0b6e6ca4e89e10941f66621b6b7555a436
droidhackzor <gentooguy@gmail.com>
2026-04-06 20:09:43 -0600

    Update migration.sql

--- prisma/migrations/20260407_add_user_name/migration.sql
+++ prisma/migrations/20260407_add_user_name/migration.sql
@@ -1,14 +1,2 @@
 ALTER TABLE "User"
 ADD COLUMN IF NOT EXISTS "name" TEXT;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM information_schema.columns
-    WHERE table_name = 'User'
-      AND column_name = 'fullName'
-  ) THEN
-    EXECUTE 'UPDATE "User" SET "name" = "fullName" WHERE "name" IS NULL AND "fullName" IS NOT NULL';
-  END IF;
-END $$;
